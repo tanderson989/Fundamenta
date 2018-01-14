7528c875fad41eebaa2f0be95ca957ca10f84ddf
Team Quicken Loans <cse498@cse498-1002-mozilla.cse.msu.edu>
2018-01-14 12:35:49 -0500

    React boilerplate set up

---
+++ app/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class HelloWorld extends React.Component {
+    constructor() {
+        super();
+    }
+
+    render() {
+        return (
+            <div>HELLO WORLD!</div>
+        )
+    }
+}
+
+export default HelloWorld;
